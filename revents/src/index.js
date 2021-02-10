a4dd6065ec0bc4e31339d2857eee08016b7bfca4
TebelloX <tebellonamo@gmail.com>
2021-02-10 23:38:46 +0200

    structuring the project

--- revents/src/index.js
+++ revents/src/index.js
@@ -4,7 +4,7 @@ import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 
-// const rootEl = document.getElementById("root");
+const rootEl = document.getElementById("root");
 
 console.log("freeeeeeeeeee");
 
@@ -14,7 +14,7 @@ function render() {
 
 if (module.hot) {
   console.log("xxxx12");
-  module.hot.accept("./app/layout/App", function () {
+  module.hot.accept("./app/layout/App", () => {
     setTimeout(render);
   });
 }
