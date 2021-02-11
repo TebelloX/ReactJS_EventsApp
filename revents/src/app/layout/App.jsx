8fce4bf4d8c5606e9f7741da2997cb7a59a53a21
TebelloX <tebellonamo@gmail.com>
2021-02-11 13:54:52 +0200

    grid semantic ui

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -5,10 +5,6 @@ export default function App() {
   return (
     <div className='App'>
       <h1>Re-events</h1>
-      <button className='ui icon red button'>
-        <i className='user icon' /> CSS Button
-      </button>
-      <Button icon='user' content='React Button' color='green' />
     </div>
   );
 }
--- revents/src/feature/events/eventDashboard/EventDashboard.jsx
+++ revents/src/feature/events/eventDashboard/EventDashboard.jsx
@@ -7,7 +7,7 @@ export default function EventDashboard() {
             <Grid.Column width={10}>
                 <h2>Left Column</h2>
             </Grid.Column>
-            <Grid.Column width={10}>
+            <Grid.Column width={6}>
                 <h2>Right Column</h2>
             </Grid.Column>
             </Grid>
