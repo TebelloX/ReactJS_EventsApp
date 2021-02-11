c59da22aebcc7c592eb5b53cbf430cb52ca1a8a0
TebelloX <tebellonamo@gmail.com>
2021-02-11 23:54:06 +0200

    grid semantic ui

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 import EventDashboard from "../../feature/events/eventDashboard/EventDashboard";
 import NavBar from "../../feature/nav/NavBar";
 
@@ -7,7 +8,9 @@ export default function App() {
     <div>
       <h1>Re-events</h1>
       <NavBar />
-      <EventDashboard />
+      <Container>
+        <EventDashboard />
+      </Container>
     </div>
   );
 }
