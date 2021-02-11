0bb282d2844a4754ccefe98e88348276634022f6
TebelloX <tebellonamo@gmail.com>
2021-02-11 13:54:18 +0200

    grid semantic ui

--- revents/src/feature/events/eventDashboard/EventDashboard.jsx
+++ revents/src/feature/events/eventDashboard/EventDashboard.jsx
@@ -5,6 +5,10 @@ export default function EventDashboard() {
     return (
         <Grid>
             <Grid.Column width={10}>
+                <h2>Left Column</h2>
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <h2>Right Column</h2>
             </Grid.Column>
             </Grid>
     )
