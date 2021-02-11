9aea51bf16db6e7a8aad99b2b25866c7991b0024
TebelloX <tebellonamo@gmail.com>
2021-02-11 11:36:49 +0200

    structuring the project

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className='App'>
       <h1>Re-events</h1>
     </div>
   );
 }
-
-export default App;
