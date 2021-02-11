516e9eb560869618c1137bd20c47a915d5b48696
TebelloX <tebellonamo@gmail.com>
2021-02-11 16:34:21 +0200

    grid semantic ui

--- revents/src/feature/nav/NavBar.jsx
+++ revents/src/feature/nav/NavBar.jsx
@@ -1,1 +1,10 @@
 import React from "react";
+import { Menu, Container } from "semantic-ui-react";
+
+export default function NavBar() {
+  return (
+    <Menu inverted fixed='top'>
+      <Container></Container>
+    </Menu>
+  );
+}
