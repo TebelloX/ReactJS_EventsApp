f55a36e95857f14b910442e30592a687d5814498
TebelloX <tebellonamo@gmail.com>
2021-02-11 14:00:46 +0200

    grid semantic ui

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
+import EventDashboard from "../../feature/events/eventDashboard/EventDashboard";
 
 export default function App() {
   return (
     <div className='App'>
       <h1>Re-events</h1>
+      <EventDashboard />
     </div>
   );
 }
