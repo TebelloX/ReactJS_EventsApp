d76d14eb8ac960598900e1bcac02b79e1c6046b1
TebelloX <tebellonamo@gmail.com>
2021-02-11 11:38:39 +0200

    structuring the project

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div className='App'>
       <h1>Re-events</h1>
+      <button className='ui icon red button'>
+        <i className='user icon' /> CSS Button
+      </button>
     </div>
   );
 }
