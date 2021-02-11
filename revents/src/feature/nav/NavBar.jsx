1254d992d28a40c54749fe2625ff9ad30c199a2c
TebelloX <tebellonamo@gmail.com>
2021-02-11 16:48:48 +0200

    grid semantic ui

--- revents/src/feature/nav/NavBar.jsx
+++ revents/src/feature/nav/NavBar.jsx
@@ -4,7 +4,13 @@ import { Menu, Container } from "semantic-ui-react";
 export default function NavBar() {
   return (
     <Menu inverted fixed='top'>
-      <Container></Container>
+      <Container>
+        <Menu.Item header>
+          <img src='/assets/logo.png' alt='logo'></img>
+          Re-vents
+        </Menu.Item>
+        <Menu name='Events' />
+      </Container>
     </Menu>
   );
 }
