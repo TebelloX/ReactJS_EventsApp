ac44a72732d10c4857fc84ed62ee05969b46ef75
TebelloX <tebellonamo@gmail.com>
2021-02-13 18:07:52 +0200

    grid semantic ui

--- revents/src/feature/events/eventDashboard/EventDashboard.jsx
+++ revents/src/feature/events/eventDashboard/EventDashboard.jsx
@@ -1,6 +1,7 @@
 import { Grid } from "semantic-ui-react";
 import React from "react";
 import EventList from "./EventList";
+import EventForm from "../eventForm/EventForm";
 
 export default function EventDashboard() {
   return (
@@ -9,7 +10,7 @@ export default function EventDashboard() {
         <EventList />
       </Grid.Column>
       <Grid.Column width={6}>
-        <h2>Right Column</h2>
+        <EventForm />
       </Grid.Column>
     </Grid>
   );
