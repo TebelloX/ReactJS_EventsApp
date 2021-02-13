6278f19ed1656da4885732c81490da7e48a190ae
TebelloX <tebellonamo@gmail.com>
2021-02-13 18:07:08 +0200

    grid semantic ui

--- revents/src/feature/events/eventForm/EventForm.jsx
+++ revents/src/feature/events/eventForm/EventForm.jsx
@@ -12,16 +12,16 @@ export default function EventForm() {
           <input type='text' placeholder='Category' />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Event title' />
+          <input type='text' placeholder='Description' />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Event title' />
+          <input type='text' placeholder='City' />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Event title' />
+          <input type='text' placeholder='Venue' />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Event title' />
+          <input type='text' placeholder='Date' />
         </Form.Field>
       </Form>
     </Segment>
