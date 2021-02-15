53b7f411941a1a6b8d1e7a9f5e12bbd03d83c496
TebelloX <tebellonamo@gmail.com>
2021-02-15 18:45:30 +0200

    adding sample data

--- revents/src/feature/events/eventDashboard/EventDashboard.jsx
+++ revents/src/feature/events/eventDashboard/EventDashboard.jsx
@@ -1,14 +1,17 @@
 import { Grid } from "semantic-ui-react";
-import React from "react";
+import React, { useState } from "react";
 import EventList from "./EventList";
 import EventForm from "../eventForm/EventForm";
 import { sampleData } from "../../../app/api/sampleData";
 
 export default function EventDashboard() {
+  const [events, setEvents] = useState(sampleData);
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={sampleData} />
+        <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
         <EventForm />
--- revents/src/feature/events/eventDashboard/EventListAttendee.jsx
+++ revents/src/feature/events/eventDashboard/EventListAttendee.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Image, List } from "semantic-ui-react";
 
-export default function EventListAttendee() {
+export default function EventListAttendee({ attendee }) {
   return (
     <List.Item>
-      <Image size='mini' circular src='/assets/user.png' />
+      <Image size='mini' circular src={attendee.photoURL} />
     </List.Item>
   );
 }
