626fe9e6bf62600c3cedb9fdaa2e0f58bfc773f4
TebelloX <tebellonamo@gmail.com>
2021-02-15 18:34:03 +0200

    props between eventDashboard and EventList

--- revents/src/feature/events/eventDashboard/EventListItem.jsx
+++ revents/src/feature/events/eventDashboard/EventListItem.jsx
@@ -8,18 +8,18 @@ export default function EventListItem({ event }) {
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size='tiny' circular src='{event.hostPhotoURL}' />
+            <Item.Image size='tiny' circular src={event.hostPhotoURL} />
             <Item.Content>
-              <Item.Header content='{event.title}' />
-              <Item.Description>Hosted by Bob</Item.Description>
+              <Item.Header content={event.title} />
+              <Item.Description>Hosted by {event.hostedBy}</Item.Description>
             </Item.Content>
           </Item>
         </Item.Group>
       </Segment>
       <Segment>
         <span>
-          <Icon name='clock' /> Date
-          <Icon name='marker' /> Venue
+          <Icon name='clock' /> {event.date}
+          <Icon name='marker' /> {event.venue}
         </span>
       </Segment>
       <Segment secondary>
