cec78cc37ad8d5270649cf3c93a24723812ad48e
TebelloX <tebellonamo@gmail.com>
2021-02-15 12:17:23 +0200

    props between eventDashboard and EventList

--- revents/src/feature/events/eventDashboard/EventDashboard.jsx
+++ revents/src/feature/events/eventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ export default function EventDashboard() {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList event={sampleData} />
+        <EventList events={sampleData} />
       </Grid.Column>
       <Grid.Column width={6}>
         <EventForm />
