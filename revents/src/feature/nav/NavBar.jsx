3a90097b3ce7e47d0606c92345573073058cde55
TebelloX <tebellonamo@gmail.com>
2021-02-16 23:14:01 +0200

    adding state to display and hide form

--- revents/src/feature/nav/NavBar.jsx
+++ revents/src/feature/nav/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
 
-export default function NavBar() {
+export default function NavBar({ setFormOpen }) {
   return (
     <Menu inverted fixed='top'>
       <Container>
@@ -14,7 +14,12 @@ export default function NavBar() {
         </Menu.Item>
         <Menu.Item name='Events' />
         <Menu.Item>
-          <Button positive inverted content='Create Event' />
+          <Button
+            onClick={() => setFormOpen(true)}
+            positive
+            inverted
+            content='Create Event'
+          />
         </Menu.Item>
         <Menu.Item position='right'>
           <Button basic inverted content='Login' />
