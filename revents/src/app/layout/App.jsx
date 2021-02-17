4873b485c3eb4ee6cbd64747bea0b64d4f2af908
TebelloX <tebellonamo@gmail.com>
2021-02-17 23:21:10 +0200

    finalising the form

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -4,7 +4,7 @@ import EventDashboard from "../../feature/events/eventDashboard/EventDashboard";
 import NavBar from "../../feature/nav/NavBar";
 
 export default function App() {
-  const [formOpen, setFormOpen] = useState(false);
+  const [formOpen, setFormOpen] = useState(true);
   return (
     <>
       <h1>Re-events</h1>
