69415f9629244471d8fbecb54cd174e4860791db
TebelloX <tebellonamo@gmail.com>
2021-02-20 00:03:26 +0200

    creating an event

--- revents/src/feature/events/eventForm/EventForm.jsx
+++ revents/src/feature/events/eventForm/EventForm.jsx
@@ -8,6 +8,7 @@ export default function EventForm({ setFormOpen, setEvents, createEvent }) {
     description: "",
     city: "",
     date: "",
+    venue: "",
   };
 
   const [values, setValues] = useState(initialValues);
