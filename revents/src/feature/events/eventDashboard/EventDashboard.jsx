6e0b847dabcf317fa2ba9bd4476b2dd0854e4260
TebelloX <tebellonamo@gmail.com>
2021-02-19 23:52:15 +0200

    creating an event

--- revents/src/feature/events/eventDashboard/EventDashboard.jsx
+++ revents/src/feature/events/eventDashboard/EventDashboard.jsx
@@ -7,6 +7,10 @@ import { sampleData } from "../../../app/api/sampleData";
 export default function EventDashboard({ formOpen, setFormOpen }) {
   const [events, setEvents] = useState(sampleData);
 
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
+
   return (
     <Grid>
       <Grid.Column width={10}>
