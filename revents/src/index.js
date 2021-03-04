877ba6b5072d62d2199015ebd3fc7e734b7d8d9e
TebelloX <tebellonamo@gmail.com>
2021-03-05 00:31:41 +0200

    adding routing

--- revents/src/index.js
+++ revents/src/index.js
@@ -4,11 +4,17 @@ import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const rootEl = document.getElementById("root");
 
 function render() {
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
 }
 
 if (module.hot) {
