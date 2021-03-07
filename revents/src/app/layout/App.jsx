183b9aca07d4f0d9fd3af509a50c0260f6f18752
TebelloX <tebellonamo@gmail.com>
2021-03-08 00:02:59 +0200

    adding routing

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -3,6 +3,7 @@ import { Route } from "react-router";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../feature/events/eventDashboard/EventDashboard";
 import NavBar from "../../feature/nav/NavBar";
+import HomePage from "../../feature/home/HomePage";
 
 export default function App() {
   const [formOpen, setFormOpen] = useState(false);
@@ -23,6 +24,9 @@ export default function App() {
       <h1>Re-events</h1>
       <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className='main'>
+        <Route path='/' component={HomePage} />
+        <Route path='/events' component={EventDashboard} />
+        <Route path='/' component={HomePage} />
         <Route path='/' component={HomePage} />
       </Container>
     </>
