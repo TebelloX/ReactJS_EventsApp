3cef12b235eb89f98644d3ffcaa57f77419817a9
TebelloX <tebellonamo@gmail.com>
2021-03-10 23:16:52 +0200

    adding routing

--- revents/src/feature/nav/NavBar.jsx
+++ revents/src/feature/nav/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Menu, Container, Button } from "semantic-ui-react";
 
 export default function NavBar({ setFormOpen }) {
   return (
     <Menu inverted fixed='top'>
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} header>
           <img
             src='/assets/logo.png'
             alt='logo'
