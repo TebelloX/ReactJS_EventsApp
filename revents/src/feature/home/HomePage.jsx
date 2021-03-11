3ab62e39c9e9f96c6f406e99afcd26f376c90921
TebelloX <tebellonamo@gmail.com>
2021-03-11 23:38:07 +0200

    adding routing

--- revents/src/feature/home/HomePage.jsx
+++ revents/src/feature/home/HomePage.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Container, Header, Segment } from "semantic-ui-react";
 
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container>
+        <Header></Header>
+      </Container>
+    </Segment>
+  );
 }
