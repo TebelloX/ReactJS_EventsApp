4f1ac447fb917b9f9af60c42c7400502a426b60c
TebelloX <tebellonamo@gmail.com>
2021-03-12 00:11:42 +0200

    menus for authenticated and unauthenticated users

--- revents/src/feature/nav/SignedInMenu.jsx
+++ revents/src/feature/nav/SignedInMenu.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Menu, Button, Image, Dropdown } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export default function SignedOutMenu() {
   return (
     <Menu.Item position='right'>
       <Image avatar spaced='right' src='/assets/user.png' />
-      <Dropdown pointing='top left' text='Bob'></Dropdown>
+      <Dropdown pointing='top left' text='Bob'>
+        <Dropdown.Menu>
+          <Dropdown.Item
+            as={Link}
+            to='/createEvent'
+            text='Create Event'
+            icon='plus'
+          />
+        </Dropdown.Menu>
+      </Dropdown>
     </Menu.Item>
   );
 }
