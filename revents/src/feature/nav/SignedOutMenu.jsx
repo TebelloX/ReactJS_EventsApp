697bd91b695956bc36a943e47335677cac62496f
TebelloX <tebellonamo@gmail.com>
2021-03-12 00:02:21 +0200

    menus for authenticated and unauthenticated users

---
+++ revents/src/feature/nav/SignedOutMenu.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function SignedOutMenu() {
+  <Menu.Item position='right'>
+    <Button basic inverted content='Login' />
+    <Button basic inverted content='Register' style={{ marginLeft: "0.5em" }} />
+  </Menu.Item>;
+}
