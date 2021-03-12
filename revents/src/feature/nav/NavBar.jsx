62994d987413c6808627c5d307e281240e2fd3e3
TebelloX <tebellonamo@gmail.com>
2021-03-12 15:43:46 +0200

    menus for authenticated and unauthenticated users

--- revents/src/feature/nav/NavBar.jsx
+++ revents/src/feature/nav/NavBar.jsx
@@ -18,9 +18,11 @@ export default function NavBar({ setFormOpen }) {
           Re-vents
         </Menu.Item>
         <Menu.Item as={NavLink} to='/events' name='Events' />
-        <Menu.Item as={NavLink} to='/createEvent'>
-          <Button positive inverted content='Create Event' />
-        </Menu.Item>
+        {authenticated && (
+          <Menu.Item as={NavLink} to='/createEvent'>
+            <Button positive inverted content='Create Event' />
+          </Menu.Item>
+        )}
         {authenticated ? <SignedInMenu /> : <SignedOutMenu />}
       </Container>
     </Menu>
