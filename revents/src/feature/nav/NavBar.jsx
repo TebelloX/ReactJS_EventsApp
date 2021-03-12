5a2216b030015b47ba318ad7fd12b223d7ffe2fd
TebelloX <tebellonamo@gmail.com>
2021-03-12 13:58:31 +0200

    menus for authenticated and unauthenticated users

--- revents/src/feature/nav/NavBar.jsx
+++ revents/src/feature/nav/NavBar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Menu, Container, Button } from "semantic-ui-react";
 import SignedOutMenu from "./SignedOutMenu";
 import SignedInMenu from "./SignedInMenu";
 
 export default function NavBar({ setFormOpen }) {
+  const [authenticated, setAuthenticated] = useState(false);
   return (
     <Menu inverted fixed='top'>
       <Container>
