27d432d9be5189003477ba175bfd790f3e17c73f
TebelloX <tebellonamo@gmail.com>
2021-03-12 15:45:13 +0200

    menus for authenticated and unauthenticated users

--- revents/src/feature/nav/SignedOutMenu.jsx
+++ revents/src/feature/nav/SignedOutMenu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Menu, Button } from "semantic-ui-react";
 
-export default function SignedOutMenu() {
+export default function SignedOutMenu({ setAuthenticated }) {
   return (
     <Menu.Item position='right'>
       <Button basic inverted content='Login' />
